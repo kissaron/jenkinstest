f28aef7b048b7daf47430b2dc358fcd437965610
Kiss Áron <cyj2ur@inf.elte.hu>
2022-10-05 14:47:35 +0200

    tizenhetedik commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,5 +15,6 @@ public class Main {
         System.out.println("HEllo");
         System.out.println("HEllo");
         System.out.println("HEllo");
+        System.out.println("HEllo");
     }
 }
