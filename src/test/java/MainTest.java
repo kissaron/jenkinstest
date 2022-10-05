419c911d3fb56dfb1c779510bcdadfd6b16c1195
Kiss Áron <cyj2ur@inf.elte.hu>
2022-10-05 13:25:55 +0200

    tizenötödik commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void demoTestMethod() {
+    assertEquals(true,true);
+     }
+}
