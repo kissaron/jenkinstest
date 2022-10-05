504bc25016ac05159a4b881d38f5af13bf7c7e84
Kiss Áron <cyj2ur@inf.elte.hu>
2022-10-05 12:31:05 +0200

    tizennegyedik commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,5 +13,6 @@ public class Main {
         System.out.println("HEllo");
         System.out.println("HEllo");
         System.out.println("HEllo");
+        System.out.println("HEllo");
     }
 }
