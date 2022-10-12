bfc03a7f46c4ead50191d20da7e3ccc6f451c5ed
Kiss Áron <cyj2ur@inf.elte.hu>
2022-10-12 14:44:02 +0200

    huszadik  commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,5 +17,7 @@ public class Main {
         System.out.println("HEllo");
         System.out.println("HEllo");
         System.out.println("HEllo");
+        System.out.println("HEllo");
+
     }
 }
